547b139ab67a6946dd38644fea7fa8c98c86f6c3
Patryk Dobrowolski <akiioto@gmail.com>
2022-10-28 22:39:03 +0200

    should be ok

--- sap/views.py
+++ sap/views.py
@@ -1,7 +1,6 @@
 import requests
 from django.shortcuts import render
 import collections
-from .models import Nickname
 
 
 def make_statistics(lst):
@@ -14,31 +13,33 @@ def index(request):
     events_list = []
     repo_language = []
 
-    user = Nickname.objects.all()
+    if 'nickname' in request.POST:
+        user = request.POST['nickname']
+    else:
+        user = 'ValveSoftware'
 
-    if user:
-        url_user_info = 'https://api.github.com/users/{}'
-        url_user_repository_info = 'https://api.github.com/users/{}/repos'
-        url_user_events_info = 'https://api.github.com/users/{}/events'
+    url_user_info = 'https://api.github.com/users/{}'
+    url_user_repository_info = 'https://api.github.com/users/{}/repos'
+    url_user_events_info = 'https://api.github.com/users/{}/events'
 
-        user_info = requests.get(url_user_info.format(user)).json()
-        user_repository_info = requests.get(url_user_repository_info.format(user)).json()
-        user_events_info = requests.get(url_user_events_info.format(user)).json()
+    user_info = requests.get(url_user_info.format(user)).json()
+    user_repository_info = requests.get(url_user_repository_info.format(user)).json()
+    user_events_info = requests.get(url_user_events_info.format(user)).json()
 
-        for user_details in user_repository_info:
-            repo_list.append(user_details['name'])
-            repo_language.append(user_details['language'])
+    for user_details in user_repository_info:
+        repo_list.append(user_details['name'])
+        repo_language.append(user_details['language'])
 
-        for events in user_events_info:
-            if events['type'] == 'PushEvent':
-                events_list.append((events['actor']['login'], events['repo']['name']))
+    for events in user_events_info:
+        if events['type'] == 'PushEvent':
+            events_list.append((events['actor']['login'], events['repo']['name']))
 
-        user_details = {
-            'user_email': user_info['name'],
-            'user_repositories': repo_list,
-            'preferred_lang': make_statistics(repo_language),
-            'events_list': events_list
-        }
+    user_details = {
+        'user_email': user_info['name'],
+        'user_repositories': repo_list,
+        'preferred_lang': make_statistics(repo_language),
+        'events_list': events_list
+    }
 
     context = {'user_details': user_details}
 
